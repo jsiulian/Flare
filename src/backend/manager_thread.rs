b6bbfa61e7d75a305ce14002e6c3999db791de46
Julian Schmidhuber <gitlab@schmiddi.anonaddy.com>
2023-10-09 16:27:30 +0200

    fix(): Manually implemented contact sync

--- src/backend/manager_thread.rs
+++ src/backend/manager_thread.rs
@@ -50,7 +50,6 @@ enum Command {
             Result<<presage_store_sled::SledStore as presage::Store>::MessagesIter, Error>,
         >,
     ),
-    RequestContactsSync(oneshot::Sender<Result<(), Error>>),
 }
 
 impl std::fmt::Debug for Command {
@@ -281,15 +280,6 @@ impl ManagerThread {
             .expect("Command sending failed");
         receiver.await.expect("Callback receiving failed")
     }
-
-    pub async fn request_contacts_sync(&self) -> Result<(), Error> {
-        let (sender, receiver) = oneshot::channel();
-        self.command_sender
-            .send(Command::RequestContactsSync(sender))
-            .await
-            .expect("Command sending failed");
-        receiver.await.expect("Callback receiving failed")
-    }
 }
 
 async fn setup_manager(
@@ -420,9 +410,5 @@ async fn handle_command(manager: &mut Manager<Store, Registered>, command: Comma
             // XXX: Cannot format iterator.
             let _ = callback.send(manager.messages(&thread, range));
         }
-        Command::RequestContactsSync(callback) => callback
-            // .send(manager.sync_contacts().await)
-            .send(Ok(()))
-            .expect("Callback sending failed"),
     }
 }
