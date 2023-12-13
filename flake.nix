3fa5e9ce13ed0b642d883343a0f41e02d26e21f1
Julian Schmidhuber <gitlab@schmiddi.anonaddy.com>
2023-12-13 19:45:24 +0100

    chores(): Update presage

--- flake.nix
+++ flake.nix
@@ -22,8 +22,8 @@
                 outputHashes = {
                   "curve25519-dalek-4.0.0" = "sha256-KUXvYXeVvJEQ/+dydKzXWCZmA2bFa2IosDzaBL6/Si0=";
                   "libsignal-protocol-0.1.0" = "sha256-FCrJO7porlY5FrwZ2c67UPd4tgN7cH2/3DTwfPjihwM=";
-                  "libsignal-service-0.1.0" = "sha256-Nv0QWBnj+e3Y6vqKfR9kebyvXomWNRRJAG2OuCqREsE=";
-                  "presage-0.6.0-dev" = "sha256-mfWr2AWCbgo2YivWGd5wRonYnX2KsLo55PAkhu2jOGc=";
+                  "libsignal-service-0.1.0" = "sha256-IKRI5Q+goRKyiEc68vWRnF10uToZz2nS9xHCyV/WcKI=";
+                  "presage-0.6.0-dev" = "sha256-/WcGqFXijR/C0/Rh91muo3cG8uwPYg9PGkNehrOZEug=";
                 };
               };
               src = let fs = lib.fileset; in fs.toSource {
